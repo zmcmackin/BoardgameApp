759379980f7fe121f64e5367c342455e1ca20cf9
alycecil <alycecil@gmail.com>
2017-01-06 11:36:39 -0500

    added BoardComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,46 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
-import { AngularFireModule } from 'angularfire2';
+import {NgModule, ErrorHandler} from '@angular/core';
+import {IonicApp, IonicModule, IonicErrorHandler} from 'ionic-angular';
+import {MyApp} from './app.component';
+import {AboutPage} from '../pages/about/about';
+import {ContactPage} from '../pages/contact/contact';
+import {HomePage} from '../pages/home/home';
+import {TabsPage} from '../pages/tabs/tabs';
+import {AngularFireModule} from 'angularfire2';
 import {BoardPage} from "../pages/board/board";
+import {BoardComponent} from "../components/board/board";
 
 export const firebaseConfig = {
-    apiKey: "AIzaSyCtTaqrwStj8yJRpGaeOf7h74Qx-e5WkwQ",
-    authDomain: "boardgameapp-2beef.firebaseapp.com",
-    databaseURL: "https://boardgameapp-2beef.firebaseio.com",
-    storageBucket: "boardgameapp-2beef.appspot.com",
-    messagingSenderId: "124060570844"
+	apiKey: "AIzaSyCtTaqrwStj8yJRpGaeOf7h74Qx-e5WkwQ",
+	authDomain: "boardgameapp-2beef.firebaseapp.com",
+	databaseURL: "https://boardgameapp-2beef.firebaseio.com",
+	storageBucket: "boardgameapp-2beef.appspot.com",
+	messagingSenderId: "124060570844"
 };
 
 @NgModule({
-  declarations: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage,
-    BoardPage
-  ],
-  imports: [
-    IonicModule.forRoot(MyApp),
-    AngularFireModule.initializeApp(firebaseConfig)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage,
-	  BoardPage
-  ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+	declarations: [
+		MyApp,
+		AboutPage,
+		ContactPage,
+		HomePage,
+		TabsPage,
+		BoardPage,
+		BoardComponent
+	],
+	imports: [
+		IonicModule.forRoot(MyApp),
+		AngularFireModule.initializeApp(firebaseConfig)
+	],
+	bootstrap: [IonicApp],
+	entryComponents: [
+		MyApp,
+		AboutPage,
+		ContactPage,
+		HomePage,
+		TabsPage,
+		BoardPage
+	],
+	providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })
-export class AppModule {}
+export class AppModule {
+}
