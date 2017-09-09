a725c540e00f5dcc1bb70200d95233fe6f09f461
The Cecil <alycecil@gmail.com>
2017-09-08 22:23:20 -0400

    Create WINDOWS.bat

---
+++ WINDOWS.bat
@@ -0,0 +1,22 @@
+## Setup from fresh pc
+# created on Windows 7
+
+#install choco
+echo install choco : https://chocolatey.org/install
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+echo installed choco
+refreshenv
+
+#install reqs
+echo installs yarn, node
+choco install yarn
+
+echo reqs needed
+choco install mongodb
+
+choco install jdk8
+
+
+
+
